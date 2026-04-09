5c458f014dac32784b68720f079a305ac2203c6b
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2026-04-09 16:05:06 +0200

    refactor: split loop stmt lowering

---
+++ src/codegen/stmt/control_flow/loops/iterative.rs
@@ -0,0 +1,114 @@
+use crate::codegen::context::{Context, LoopLabels};
+use crate::codegen::data_section::DataSection;
+use crate::codegen::emit::Emitter;
+use crate::codegen::expr::emit_expr;
+use crate::parser::ast::{Expr, Stmt};
+
+pub(crate) fn emit_do_while_stmt(
+    body: &[Stmt],
+    condition: &Expr,
+    emitter: &mut Emitter,
+    ctx: &mut Context,
+    data: &mut DataSection,
+) {
+    let loop_start = ctx.next_label("dowhile_start");
+    let loop_end = ctx.next_label("dowhile_end");
+    let loop_cond = ctx.next_label("dowhile_cond");
+
+    emitter.blank();
+    emitter.comment("do...while");
+    emitter.label(&loop_start);
+
+    ctx.loop_stack.push(LoopLabels {
+        continue_label: loop_cond.clone(),
+        break_label: loop_end.clone(),
+        sp_adjust: 0,
+    });
+    for s in body {
+        super::super::super::emit_stmt(s, emitter, ctx, data);
+    }
+    ctx.loop_stack.pop();
+
+    emitter.label(&loop_cond);
+    let cond_ty = emit_expr(condition, emitter, ctx, data);
+    crate::codegen::expr::coerce_to_truthiness(emitter, ctx, &cond_ty);
+    crate::codegen::abi::emit_branch_if_int_result_nonzero(emitter, &loop_start);
+    emitter.label(&loop_end);
+}
+
+pub(crate) fn emit_while_stmt(
+    condition: &Expr,
+    body: &[Stmt],
+    emitter: &mut Emitter,
+    ctx: &mut Context,
+    data: &mut DataSection,
+) {
+    let loop_start = ctx.next_label("while_start");
+    let loop_end = ctx.next_label("while_end");
+
+    emitter.blank();
+    emitter.comment("while");
+    emitter.label(&loop_start);
+    let cond_ty = emit_expr(condition, emitter, ctx, data);
+    crate::codegen::expr::coerce_to_truthiness(emitter, ctx, &cond_ty);
+    crate::codegen::abi::emit_branch_if_int_result_zero(emitter, &loop_end);
+
+    ctx.loop_stack.push(LoopLabels {
+        continue_label: loop_start.clone(),
+        break_label: loop_end.clone(),
+        sp_adjust: 0,
+    });
+    for s in body {
+        super::super::super::emit_stmt(s, emitter, ctx, data);
+    }
+    ctx.loop_stack.pop();
+
+    emitter.instruction(&format!("b {}", loop_start));                             // unconditional branch back to loop start
+    emitter.label(&loop_end);
+}
+
+pub(crate) fn emit_for_stmt(
+    init: &Option<Box<Stmt>>,
+    condition: &Option<Expr>,
+    update: &Option<Box<Stmt>>,
+    body: &[Stmt],
+    emitter: &mut Emitter,
+    ctx: &mut Context,
+    data: &mut DataSection,
+) {
+    let loop_start = ctx.next_label("for_start");
+    let loop_continue = ctx.next_label("for_cont");
+    let loop_end = ctx.next_label("for_end");
+
+    emitter.blank();
+    emitter.comment("for");
+
+    if let Some(s) = init {
+        super::super::super::emit_stmt(s, emitter, ctx, data);
+    }
+
+    emitter.label(&loop_start);
+
+    if let Some(cond) = condition {
+        let cond_ty = emit_expr(cond, emitter, ctx, data);
+        crate::codegen::expr::coerce_to_truthiness(emitter, ctx, &cond_ty);
+        crate::codegen::abi::emit_branch_if_int_result_zero(emitter, &loop_end);
+    }
+
+    ctx.loop_stack.push(LoopLabels {
+        continue_label: loop_continue.clone(),
+        break_label: loop_end.clone(),
+        sp_adjust: 0,
+    });
+    for s in body {
+        super::super::super::emit_stmt(s, emitter, ctx, data);
+    }
+    ctx.loop_stack.pop();
+
+    emitter.label(&loop_continue);
+    if let Some(s) = update {
+        super::super::super::emit_stmt(s, emitter, ctx, data);
+    }
+    emitter.instruction(&format!("b {}", loop_start));                             // unconditional branch back to loop start
+    emitter.label(&loop_end);
+}
