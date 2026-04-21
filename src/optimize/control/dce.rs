b3f38073e93b910f620619cf92e656c21635e883
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2026-04-21 21:16:45 +0200

    Merge catches exposed by shadow drop

--- src/optimize/control/dce.rs
+++ src/optimize/control/dce.rs
@@ -449,7 +449,7 @@ fn dce_try_stmt(
         })
         .collect();
     let catches = if block_may_throw(&try_body) {
-        drop_shadowed_catch_clauses(normalize_catch_clauses(catches))
+        normalize_catch_clauses(drop_shadowed_catch_clauses(normalize_catch_clauses(catches)))
     } else {
         Vec::new()
     };
@@ -511,7 +511,7 @@ fn dce_try_stmt_with_tail(
         })
         .collect();
     let catches = if block_may_throw(&try_body) {
-        drop_shadowed_catch_clauses(normalize_catch_clauses(catches))
+        normalize_catch_clauses(drop_shadowed_catch_clauses(normalize_catch_clauses(catches)))
     } else {
         Vec::new()
     };
--- src/optimize/tests.rs
+++ src/optimize/tests.rs
@@ -2463,6 +2463,61 @@ fn test_eliminate_dead_code_drops_duplicate_shadowed_catch_types() {
 }
 
 #[test]
+fn test_eliminate_dead_code_merges_identical_catches_exposed_by_shadow_drop() {
+    let program = vec![Stmt::new(
+        StmtKind::FunctionDecl {
+            name: "main".into(),
+            params: Vec::new(),
+            variadic: None,
+            return_type: None,
+            body: vec![Stmt::new(
+                StmtKind::Try {
+                    try_body: vec![Stmt::new(
+                        StmtKind::Throw(Expr::string_lit("boom")),
+                        Span::dummy(),
+                    )],
+                    catches: vec![
+                        crate::parser::ast::CatchClause {
+                            exception_types: vec!["Exception".into()],
+                            variable: Some("e".into()),
+                            body: vec![Stmt::echo(Expr::int_lit(7))],
+                        },
+                        crate::parser::ast::CatchClause {
+                            exception_types: vec!["Exception".into()],
+                            variable: Some("shadowed".into()),
+                            body: vec![Stmt::echo(Expr::int_lit(8))],
+                        },
+                        crate::parser::ast::CatchClause {
+                            exception_types: vec!["Error".into()],
+                            variable: Some("e".into()),
+                            body: vec![Stmt::echo(Expr::int_lit(7))],
+                        },
+                    ],
+                    finally_body: None,
+                },
+                Span::dummy(),
+            )],
+        },
+        Span::dummy(),
+    )];
+
+    let eliminated = eliminate_dead_code(program);
+
+    let StmtKind::FunctionDecl { body, .. } = &eliminated[0].kind else {
+        panic!("expected function");
+    };
+    let StmtKind::Try { catches, .. } = &body[0].kind else {
+        panic!("expected try");
+    };
+    assert_eq!(catches.len(), 1);
+    assert_eq!(catches[0].variable.as_deref(), Some("e"));
+    assert_eq!(catches[0].body, vec![Stmt::echo(Expr::int_lit(7))]);
+    assert_eq!(catches[0].exception_types.len(), 2);
+    assert_eq!(catches[0].exception_types[0].as_str(), "Error");
+    assert_eq!(catches[0].exception_types[1].as_str(), "Exception");
+}
+
+#[test]
 fn test_eliminate_dead_code_reduces_empty_if_chain_to_needed_condition_checks() {
     let touch = Expr::new(
         ExprKind::FunctionCall {
