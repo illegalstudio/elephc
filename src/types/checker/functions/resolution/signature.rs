6458146ff33134f993a4e08d6ea0fa975b94de4f
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2026-05-10 17:26:55 +0200

    refactor: split function resolution checking

---
+++ src/types/checker/functions/resolution/signature.rs
@@ -0,0 +1,141 @@
+use std::collections::HashMap;
+
+use crate::errors::CompileError;
+use crate::types::{FunctionSig, PhpType, TypeEnv};
+
+use super::super::super::{Checker, FnDecl};
+
+impl Checker {
+    pub(crate) fn resolve_function_signature(
+        &mut self,
+        name: &str,
+        decl: &FnDecl,
+        param_types: Vec<(String, PhpType)>,
+    ) -> Result<PhpType, CompileError> {
+        let mut local_env: TypeEnv = HashMap::new();
+        for (pname, pty) in &param_types {
+            local_env.insert(pname.clone(), pty.clone());
+        }
+
+        let provisional_sig = FunctionSig {
+            params: param_types.clone(),
+            defaults: decl.defaults.clone(),
+            return_type: PhpType::Int,
+            declared_return: decl.return_type.is_some(),
+            ref_params: decl.ref_params.clone(),
+            declared_params: decl
+                .param_types
+                .iter()
+                .map(|type_ann| type_ann.is_some())
+                .chain(decl.variadic.iter().map(|_| false))
+                .collect(),
+            variadic: decl.variadic.clone(),
+        };
+        self.functions.insert(name.to_string(), provisional_sig);
+
+        let mut return_type = PhpType::Void;
+        let mut all_return_infos = Vec::new();
+        let mut errors = Vec::new();
+        let ref_param_names: Vec<String> = decl
+            .params
+            .iter()
+            .zip(decl.ref_params.iter())
+            .filter(|(_, is_ref)| **is_ref)
+            .map(|(name, _)| name.clone())
+            .collect();
+        self.with_local_storage_context(ref_param_names, |checker| {
+            for stmt in &decl.body {
+                if let Err(error) = checker.check_stmt(stmt, &mut local_env) {
+                    errors.extend(error.flatten());
+                }
+                checker.collect_return_infos(stmt, &local_env, &mut all_return_infos);
+            }
+            Ok(())
+        })?;
+        if !errors.is_empty() {
+            return Err(CompileError::from_many(errors));
+        }
+
+        if let Some(type_ann) = decl.return_type.as_ref() {
+            let declared_ret = self.resolve_declared_return_type_hint(
+                type_ann,
+                decl.span,
+                &format!("Function '{}'", name),
+            )?;
+            if matches!(declared_ret, PhpType::Never) && Self::body_contains_return(&decl.body) {
+                return Err(CompileError::new(
+                    decl.span,
+                    &format!("Function '{}' declared never must not return", name),
+                ));
+            }
+            self.require_declared_return_coverage(
+                &declared_ret,
+                &decl.body,
+                decl.span,
+                &format!("Function '{}'", name),
+            )?;
+            if !all_return_infos.is_empty() {
+                for return_info in &all_return_infos {
+                    self.require_compatible_return_type(
+                        &declared_ret,
+                        &return_info.ty,
+                        return_info.has_value,
+                        decl.span,
+                        &format!("Function '{}' return type", name),
+                    )?;
+                }
+            }
+            return_type = if Self::is_generic_array_hint(&declared_ret)
+                && matches!(inferred_specific_array_type_from_infos(&all_return_infos), Some(_))
+            {
+                inferred_specific_array_type_from_infos(&all_return_infos).unwrap()
+            } else {
+                declared_ret
+            };
+        } else if !all_return_infos.is_empty() {
+            return_type = all_return_infos[0].ty.clone();
+            for return_info in &all_return_infos[1..] {
+                return_type = Self::wider_type(&return_type, &return_info.ty);
+            }
+        }
+
+        let sig = FunctionSig {
+            params: param_types,
+            defaults: decl.defaults.clone(),
+            return_type: return_type.clone(),
+            declared_return: decl.return_type.is_some(),
+            ref_params: decl.ref_params.clone(),
+            declared_params: decl
+                .param_types
+                .iter()
+                .map(|type_ann| type_ann.is_some())
+                .chain(decl.variadic.iter().map(|_| false))
+                .collect(),
+            variadic: decl.variadic.clone(),
+        };
+        self.functions.insert(name.to_string(), sig);
+
+        Ok(return_type)
+    }
+}
+
+fn inferred_specific_array_type_from_infos(
+    return_types: &[super::super::returns::ReturnInfo],
+) -> Option<PhpType> {
+    let mut specific: Option<PhpType> = None;
+    for return_info in return_types {
+        let return_ty = &return_info.ty;
+        if matches!(return_ty, PhpType::Void) {
+            continue;
+        }
+        if !matches!(return_ty, PhpType::Array(_) | PhpType::AssocArray { .. }) {
+            return None;
+        }
+        match &specific {
+            None => specific = Some(return_ty.clone()),
+            Some(existing) if existing == return_ty => {}
+            _ => return None,
+        }
+    }
+    specific
+}
---
+++ src/types/checker/functions/resolution/specialization.rs
@@ -0,0 +1,61 @@
+use crate::errors::CompileError;
+use crate::parser::ast::{Expr, ExprKind};
+use crate::types::{PhpType, TypeEnv};
+
+use super::super::super::Checker;
+
+impl Checker {
+    pub(crate) fn specialize_untyped_function_params(
+        &mut self,
+        name: &str,
+        args: &[Expr],
+        caller_env: &TypeEnv,
+    ) -> Result<(), CompileError> {
+        let actual_arg_types = args
+            .iter()
+            .map(|arg| self.infer_type(arg, caller_env))
+            .collect::<Result<Vec<_>, CompileError>>()?;
+        if let Some(stored_sig) = self.functions.get_mut(name) {
+            let regular_param_count = crate::types::call_args::regular_param_count(stored_sig);
+            let mut seen_idx = 0usize;
+            for (arg, actual_ty) in args.iter().zip(actual_arg_types.iter()) {
+                if matches!(arg.kind, ExprKind::Spread(_)) {
+                    continue;
+                }
+                if seen_idx < regular_param_count
+                    && !stored_sig
+                        .declared_params
+                        .get(seen_idx)
+                        .copied()
+                        .unwrap_or(false)
+                    && stored_sig.params[seen_idx].1 == PhpType::Int
+                    && *actual_ty != PhpType::Int
+                {
+                    stored_sig.params[seen_idx].1 = actual_ty.clone();
+                }
+                seen_idx += 1;
+            }
+            if stored_sig.variadic.is_some() && seen_idx > regular_param_count {
+                let regular_names: Vec<String> = stored_sig.params[..regular_param_count]
+                    .iter()
+                    .map(|(name, _)| name.clone())
+                    .collect();
+                if Self::has_unknown_named_variadic_arg(args, &regular_names) {
+                    if let Some((_, variadic_ty)) = stored_sig.params.last_mut() {
+                        *variadic_ty = PhpType::Iterable;
+                    }
+                } else {
+                    let mut elem_ty = actual_arg_types[regular_param_count].clone();
+                    for actual_ty in actual_arg_types.iter().skip(regular_param_count + 1) {
+                        elem_ty = Self::wider_type(&elem_ty, actual_ty);
+                    }
+                    elem_ty = Self::variadic_container_elem_ty(elem_ty);
+                    if let Some((_, PhpType::Array(existing_elem_ty))) = stored_sig.params.last_mut() {
+                        **existing_elem_ty = Self::wider_type(existing_elem_ty.as_ref(), &elem_ty);
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+}
