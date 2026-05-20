c85f9825f040b0e4745549af2c97ff21a624c303
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2026-05-20 15:17:08 +0200

    test: cover pointer arguments to user methods

--- tests/codegen/pointers.rs
+++ tests/codegen/pointers.rs
@@ -300,6 +300,32 @@ free($buf);
 }
 
 #[test]
+fn test_ptr_argument_to_user_method_preserves_pointer_value() {
+    let out = compile_and_run(
+        r#"<?php
+extern "System" {
+    function malloc(int $size): ptr;
+    function free(ptr $p): void;
+}
+class Writer {
+    public function run(): void {
+        $mem = malloc(64);
+        $this->write($mem, 999);
+        echo ptr_read32($mem);
+        free($mem);
+    }
+    public function write(ptr $p, int $value): void {
+        ptr_write32($p, $value);
+    }
+}
+$writer = new Writer();
+$writer->run();
+"#,
+    );
+    assert_eq!(out, "999");
+}
+
+#[test]
 fn test_ptr_offset_computed_local_before_write32() {
     let out = compile_and_run(
         r#"<?php
