2f677217be88aa33a28fb7cb596bb8704cfd566f
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2026-06-16 17:39:22 +0200

    Support eval preg_match_all captures

--- tests/codegen/eval.rs
+++ tests/codegen/eval.rs
@@ -2314,6 +2314,10 @@ eval('$ok = preg_match("/([a-z]+)([0-9]+)/", "id42", $matches);
 echo $ok . ":" . count($matches) . ":" . $matches[0] . ":" . $matches[1] . ":" . $matches[2] . ":";
 echo preg_match("/xyz/", "id42") . ":";
 echo preg_match_all("/[0-9]+/", "a1b22c333") . ":";
+$allCount = preg_match_all("/([a-z]+)([0-9]+)/", "a1 b22", $all);
+echo $allCount . ":" . count($all) . ":" . $all[0][1] . ":" . $all[1][0] . ":" . $all[2][1] . ":";
+preg_match_all("/(x)(y)/", "abc", $none);
+echo count($none) . ":" . count($none[0]) . ":" . count($none[1]) . ":" . count($none[2]) . ":";
 echo preg_replace("/([a-z])([0-9])/", "$2-$1", "a1 b2") . ":";
 function eval_regex_wrap($matches) { return "[" . $matches[0] . "]"; }
 echo preg_replace_callback("/[A-Z]/", "eval_regex_wrap", "AB") . ":";
@@ -2331,7 +2335,7 @@ echo function_exists("preg_match") && function_exists("preg_match_all") && funct
     );
     assert_eq!(
         out,
-        "1:3:id42:id:42:0:3:1-a 2-b:[A][B]:2:a:b,c:2:b:1:aN:3:,:1"
+        "1:3:id42:id:42:0:3:2:3:b22:a:22:3:0:0:0:1-a 2-b:[A][B]:2:a:b,c:2:b:1:aN:3:,:1"
     );
 }
 
